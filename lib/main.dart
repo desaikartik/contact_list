5b6f8e616013fda8106c52a921b6bcd0e7a24652
Desai Riddhi <desairiddhi15@gmail.com>
2022-05-29 07:34:27 +0530

    change code

--- lib/main.dart
+++ lib/main.dart
@@ -1,87 +1,324 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
 }
 
+String baseUrl = 'https://628dd82ca339dfef87a17642.mockapi.io';
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: MyHomePage(),
     );
   }
 }
 
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({
+    Key? key,
+  }) : super(key: key);
+
+  //final String title;
 
   @override
-  State<HomePage> createState() => _HomePageState();
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _MyHomePageState extends State<MyHomePage> {
+  List<User> users = [];
+  User? user;
+  bool _isLoading = false;
+
+  @override
+  void initState() {
+    getUsers();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black87,
       appBar: AppBar(
-        title: const Text(
-          'Contacts',
-          style: TextStyle(fontSize: 30, fontWeight: FontWeight.normal),
+        backgroundColor: Colors.black87,
+        title:const  Text(
+          'Contact',
+          style:  TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
         ),
-        actions: const [
-          Icon(
-            Icons.search,
-            size: 30,
-          )
+elevation: 0,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.star_border),
+            onPressed: () {},
+          ),
+          const SizedBox(width: 15),
+          IconButton(
+            icon: const Icon(Icons.search),
+            onPressed: () {},
+          ),
+          const SizedBox(width: 15),
+          IconButton(
+            icon: const Icon(Icons.more_vert),
+            onPressed: () {},
+          ),
         ],
       ),
-      body: ListView(
+      floatingActionButton: FloatingActionButton(onPressed: (){},child: const Icon(Icons.add,color: Colors.white,size: 30,),),
+      body: _isLoading
+          ? const Center(child: CircularProgressIndicator.adaptive())
+          : ListView.builder(
+              itemCount: users.length,
+              itemBuilder: (context, index) => Card(
+                color: Colors.black45,
+                
+                child: ListTile(
+                  onTap: () {
+                    //inavigate with id
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (BuildContext context) =>
+                                DetailPage(id: users[index].id)));
+                  },
+                  
+                  leading:
+                      //CircleAvatar(
+                      //child:  ClipRRect(child: Image.network('${users[index].url}',fit: BoxFit.cover,)),),
+                      const Icon(
+                    Icons.person,
+                    color: Colors.white,
+                  ),
+                  title: Text(
+                    '${users[index].name}',
+                    style: const TextStyle(color: Colors.white),
+                  ),
+                  trailing: const Icon(
+                    Icons.phone,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+    );
+  }
+
+  void getUsers() async {
+    setState(() => _isLoading = true);
+    http.Response res = await http.get(Uri.parse('$baseUrl/users/'));
+
+    if (res.statusCode == 200) {
+      var decoded = jsonDecode(res.body);
+      decoded.forEach((v) {
+        setState(() {
+          users.add(User(
+            id: v['id'],
+            name: v['name'],
+            url: v['avatar'],
+            createdAt: v['createdAt'],
+          ));
+        });
+      });
+      setState(() => _isLoading = false);
+    }
+  }
+}
+
+// model
+class User {
+  final String? id;
+  final String? name;
+  final String? url;
+  final String? createdAt;
+
+  User({this.id, this.name, this.url, this.createdAt});
+}
+
+class DetailPage extends StatefulWidget {
+  final String? id;
+  const DetailPage({Key? key, this.id}) : super(key: key);
+
+  @override
+  State<DetailPage> createState() => _DetailPageState();
+}
+
+class _DetailPageState extends State<DetailPage> {
+  User? user;
+
+  @override
+  void initState() {
+    super.initState();
+    getUser();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(96, 89, 83, 83),
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 220, 175, 111),
+        elevation: 0,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.star_border),
+            onPressed: () {},
+          ),
+          const SizedBox(width: 15),
+          IconButton(
+            icon: const Icon(Icons.edit),
+            onPressed: () {},
+          ),
+          const SizedBox(width: 15),
+          IconButton(
+            icon: const Icon(Icons.more_vert),
+            onPressed: () {},
+          ),
+        ],
+        // leading: Icon(Icons.more_horiz),
+      ),
+      body: Column(
         children: [
-          _listItem(name: 'Ridhi', gmail: 'ridhi16@gmail.com'),
-          _listItem(name: 'Mansi', gmail: 'mansi126@gmail.com'),
-          _listItem(name: 'Shhradha', gmail: 'shhradha1116@gmail.com'),
-          _listItem(name: 'Tulsi', gmail: 'tulsi346@gmail.com'),
-          _listItem(name: 'Nilesh sir', gmail: 'nilesh786@gmail.com'),
-          _listItem(name: 'Paresh sir', gmail: 'paresh987@gmail.com'),
-          _listItem(name: 'Janvi', gmail: 'janvi456@gmail.com'),
-          _listItem(name: 'Adnan sir', gmail: 'adnan566@gmail.com'),
-          _listItem(name: 'Bhoomika', gmail: 'bhoomi456@gmail.com'),
-          _listItem(name: 'Kartik', gmail: 'kartik07@gmail.com'),
-          _listItem(name: 'Khushbu', gmail: 'khushi09@gmail.com'),
-          _listItem(name: 'mumma', gmail: 'bharti09@gmail.com'),
-          _listItem(name: 'mansi', gmail: 'mansi65@gmail.com'),
-          _listItem(name: 'pa', gmail: 'keshu45@gmail.com'),
-          _listItem(name: 'dadu'),
+          Container(
+            height: 270,
+            width: double.infinity,
+            decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 220, 175, 111),
+                borderRadius: BorderRadius.circular(3)),
+            child: Center(
+              child: Padding(
+                padding: const EdgeInsets.only(top: 70),
+                child:
+                    Text("${user?.name}", style: const TextStyle(fontSize: 25)),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 30, right: 10, left: 10),
+            child: Row(
+              children: const [
+                Icon(
+                  Icons.phone,
+                  color: Colors.green,
+                  size: 30,
+                ),
+                SizedBox(
+                  width: 30,
+                ),
+                Text(
+                  'Phone',
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+                SizedBox(width: 180),
+                Icon(
+                  Icons.video_call,
+                  color: Colors.green,
+                  size: 30,
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Icon(
+                  Icons.message,
+                  color: Colors.blue,
+                  size: 30,
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 70, right: 20, left: 20),
+            child: Column(
+              children: [
+                Row(
+                  children: const [
+                    Icon(
+                      Icons.whatsapp,
+                      color: Colors.green,
+                      size: 30,
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Text(
+                      'Voice Call',
+                      style: TextStyle(color: Colors.white, fontSize: 17),
+                    )
+                  ],
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
+                Row(
+                  children: const [
+                    Icon(
+                      Icons.whatsapp,
+                      color: Colors.green,
+                      size: 30,
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Text(
+                      'Video Call',
+                      style: TextStyle(color: Colors.white, fontSize: 17),
+                    )
+                  ],
+                ),
+                const SizedBox(height: 40),
+                Row(
+                  children: const [
+                    Icon(
+                      Icons.whatsapp,
+                      color: Colors.green,
+                      size: 30,
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Text(
+                      'Message',
+                      style: TextStyle(color: Colors.white, fontSize: 17),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.only(top: 100, right: 150),
+            child: Text(
+              'QR code business card',
+              style: TextStyle(color: Colors.white, fontSize: 20),
+            ),
+          )
         ],
       ),
     );
   }
-}
 
-Widget _listItem({String? name, String? gmail}) {
-  return Card(
-    color: Colors.amber[300],
-    child: ListTile(
-        dense: true,
-        onTap: () => ("$name pressed"),
-        leading: const Icon(Icons.person),
-        //  CircleAvatar(
-        //   child: ClipRRect(
-        //     borderRadius: BorderRadius.circular(30),
-        //     ),
-        // ),
-        title: Text(
-          "$name",
-          style: const TextStyle(fontSize: 20),
-        ),
-        subtitle: Text(
-          "$gmail",
-          style: const TextStyle(fontWeight: FontWeight.w800),
-        ),
-        trailing: const Icon(Icons.phone)),
-  );
+  void getUser() async {
+    http.Response res =
+        await http.get(Uri.parse('$baseUrl/users/${widget.id}'));
+
+    print(widget.id);
+    print(res.statusCode);
+    if (res.statusCode == 200) {
+      var decoded = jsonDecode(res.body);
+      setState(() {
+        user = User(
+          name: decoded['name'],
+          url: decoded['avatar'],
+          createdAt: decoded['createdAt'],
+        );
+      });
+    }
+  }
 }
